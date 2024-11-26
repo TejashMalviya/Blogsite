2c850c7e180fd70b8b5a7152aa916b17e1faa481
Tejash kumar Luhar <136250960+TejashMalviya@users.noreply.github.com>
2024-11-26 15:26:25 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+let firebaseConfig = {
+apiKey: "AIzaSyCrS6Q3Wx2OYYLSqX-820RlyJyCXwyEXOg",
+  authDomain: "blogsite-b7c33.firebaseapp.com",
+  projectId: "blogsite-b7c33",
+  storageBucket: "blogsite-b7c33.firebasestorage.app",
+  messagingSenderId: "869103104966",
+  appId: "1:869103104966:web:ead07783e24a5ecc0738c7",
+  measurementId: "G-DMFLJJWRJ1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
